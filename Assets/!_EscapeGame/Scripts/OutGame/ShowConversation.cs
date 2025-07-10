4eaecf94326908e182f2ddce10df1498451b52d6
Keigo77 <kake32059kig@gmail.com>
2025-07-10 13:42:09 +0900

    feat: ページ送りの三角形を，クリックまで点滅させる

--- Assets/!_EscapeGame/Scripts/OutGame/ShowConversation.cs
+++ Assets/!_EscapeGame/Scripts/OutGame/ShowConversation.cs
@@ -11,12 +11,15 @@ public class ShowConversation : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _conversationText;
     [SerializeField] private int _maxTextLine;
     public static float AddTextSpan = 0.05f;     // ページ送りの速度を設置
+    private CancellationTokenSource _ctsToken;
     private CancellationToken _token;
     private string[] _fileTexts;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        _token = this.GetCancellationTokenOnDestroy();
+        _ctsToken = new CancellationTokenSource();
         _fileTexts = _conversationTextFile.text.Split(char.Parse("\n"));
         ShowText().Forget();
     }
@@ -27,14 +30,27 @@ public class ShowConversation : MonoBehaviour
         
         while (textsIndex < _fileTexts.Length)
         {
+            await UniTask.WaitUntil(() => !_ctsToken.IsCancellationRequested, cancellationToken: _token);
             for (int j = 0; j < _fileTexts[textsIndex].Length; j++)
             {
+                if (j < _fileTexts[textsIndex].Length && _fileTexts[textsIndex][j] == '\\' &&
+                    _fileTexts[textsIndex][j + 1] == 'n')
+                {
+                    _conversationText.text += "\n";
+                    j++;
+                    continue;
+                }
                 _conversationText.text += _fileTexts[textsIndex][j];
                 await UniTask.Delay(TimeSpan.FromSeconds(AddTextSpan), cancellationToken: _token);
             }
-            _conversationText.text += "\n";
+            
             // クリックを待つ
+            ShowTriangles().Forget();
             await UniTask.WaitUntil(() => Input.GetMouseButtonDown(0), cancellationToken: _token);
+            _ctsToken.Cancel();
+            
+            _conversationText.text = _conversationText.text.Replace("▼", "");  // ▼だけ消す
+            _conversationText.text += "\n";
             textsIndex++;
 
             if (_conversationText.textInfo.lineCount > _maxTextLine)
@@ -43,5 +59,17 @@ public class ShowConversation : MonoBehaviour
             }
         }
         // シーン遷移
+    }
+
+    private async UniTask ShowTriangles()
+    {
+        while (!_ctsToken.IsCancellationRequested)
+        {
+            _conversationText.text += '▼';
+            await UniTask.Delay(TimeSpan.FromSeconds(0.3f), cancellationToken: _token);
+            _conversationText.text = _conversationText.text.Replace("▼", "");  // ▼だけ消す
+            await UniTask.Delay(TimeSpan.FromSeconds(0.3f), cancellationToken: _token);
+        }
+        _ctsToken = new CancellationTokenSource();
     }
 }
