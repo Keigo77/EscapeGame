9ca9e17f1ef1f498d85636e188c8160551fcca30
Keigo77 <kake32059kig@gmail.com>
2025-10-12 16:08:06 +0900

    レビューの表示を，脱出後1度だけする

--- Assets/!_EscapeGame/Scripts/OutGame/TitleManager.cs
+++ Assets/!_EscapeGame/Scripts/OutGame/TitleManager.cs
@@ -9,10 +9,11 @@ public class TitleManager : MonoBehaviour
         if (ES3.KeyExists("HaveGoaled") && ES3.Load<bool>("HaveGoaled"))
         {
             _BeforeGoalButton.SetActive(true);
+            
             #if UNITY_IOS
-            UnityEngine.iOS.Device.RequestStoreReview();
             if (!ES3.KeyExists("ShowedReview"))
             {
+                UnityEngine.iOS.Device.RequestStoreReview();
                 ES3.Save<bool>("ShowedReview", true);
             }
             #endif
